055f67e05bb82c8dbfc1c87f161cae42fbdac0bd
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-03-25 14:37:05 +0700

    Tipe data

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -10,3 +10,18 @@ void input() {
 	cin >> bilDua;
 }
 
+float penjumlahan(float a, float b) {
+	return a + b;
+}
+
+float pembagian(float a, float b) {
+	return a / b;
+}
+
+float perkalian(float a, float b) {
+	return a * b;
+}
+
+float pengurangan(float a, float b) {
+	return a - b;
+}
